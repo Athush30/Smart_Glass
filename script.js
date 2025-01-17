0cecc26390b8c9fc60384b78eb596a236d6eb35e
Athush30 <ahilakumarant.22@uom.lk>
2025-01-17 22:18:20 +0530

    commit

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+function openPage() {
+    window.location.href = "signin.html";}
+
+// Add event listener to the button
+document.addEventListener("DOMContentLoaded", () => {
+const button = document.getElementById("signin");
+button.addEventListener("click", openPage);
+});  
+// Select all header links
+const headerLinks = document.querySelectorAll('.header a');
+
+// Function to set the active class when a link is clicked
+headerLinks.forEach(link => {
+link.addEventListener('click', function () {
+// Remove active class from all links
+headerLinks.forEach(item => item.classList.remove('active'));
+
+// Add active class to the clicked link
+this.classList.add('active');
+});
+});
+
 
